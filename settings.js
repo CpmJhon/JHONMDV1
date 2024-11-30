4abe0a17db2f8f52f801d4c3e74e896abfa78cbf
CpmJhon <fariddewangga@gmail.com>
2024-11-30 15:07:56 +0700

    Welcome To Cpm_Jhon

---
+++ settings.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+/*
+	* Create By Naze
+	* Follow https://github.com/nazedev
+	* Whatsapp : https://whatsapp.com/channel/0029VaWOkNm7DAWtkvkJBK43
+*/
+
+//~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
+
+global.owner = ['6285894955362','0']
+global.packname = 'Bot WhatsApp'
+global.author = 'Jhon_dev'
+global.botname = 'Cpm_Jhon Bot'
+global.listprefix = ['+','!']
+global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
+global.tempatDB = 'database.json'
+global.pairing_code = true
+
+global.fake = {
+	anonim: 'https://telegra.ph/file/95670d63378f7f4210f03.png',
+	thumbnailUrl: 'https://telegra.ph/file/fe4843a1261fc414542c4.jpg',
+	thumbnail: fs.readFileSync('./src/media/naze.png'),
+	docs: fs.readFileSync('./src/media/fake.pdf'),
+	listfakedocs: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.openxmlformats-officedocument.presentationml.presentation','application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/pdf'],
+}
+
+global.my = {
+	yt: 'https://youtube.com/c/galeri_car_cpm8048',
+	gh: 'https://github.com/CpmJhon',
+	gc: 'https://chat.whatsapp.com/GEsjoxA3YYX7ImD3EgSBdy',
+	ch: '120363250409960161@newsletter',
+}
+
+global.limit = {
+	free: 100,
+	premium: 999,
+	vip: 'VIP'
+}
+
+global.uang = {
+	free: 10000,
+	premium: 1000000,
+	vip: 10000000
+}
+
+global.mess = {
+	key0: 'htrkey-KhR2edvhbNdQ',
+	owner: 'Fitur Khusus Owner!',
+	admin: 'Fitur Khusus Admin!',
+	botAdmin: 'Bot Bukan Admin!',
+	group: 'Gunakan Di Group!',
+	private: 'Gunakan Di Privat Chat!',
+	prem: 'Khusus User Premium!',
+	wait: 'Loading...',
+	error: 'Error!',
+	done: 'Done'
+}
+
+global.APIs = {
+	hitori: 'https://my.hitori.pw/api',
+}
+global.APIKeys = {
+	'https://my.hitori.pw/api': 'htrkey-KhR2edvhbNdQ',
+}
+
+//~~~~~~~~~~~~~~~< PROCESS >~~~~~~~~~~~~~~~\\
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+});
